8369cc46723c71ea32da7cc7020c0f08c97dd183
Dylan Camus <dylan.camus@skyward.io>
2018-06-19 17:26:38 -0700

    Added Post repository

---
+++ src/main/java/com/example/blogpostapi/repository/PostDAO.java
@@ -0,0 +1,10 @@
+package com.example.blogpostapi.repository;
+
+import com.example.blogpostapi.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostDAO extends JpaRepository<Post, Long> {
+
+}
